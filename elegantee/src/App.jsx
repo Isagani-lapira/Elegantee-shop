b0eb4719ec00e0d975721e6a8b8debc71177b58c
Isagani Lapira <lapiraisagani@gmail.com>
2024-08-22 12:25:29 +0800

    Initial Project Structure

---
+++ elegantee/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ButtonUsage from './elegantee/component/sample'
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonUsage/>
+    </div>
+  )
+}
+
+export default App
