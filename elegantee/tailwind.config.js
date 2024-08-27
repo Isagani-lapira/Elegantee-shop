56be60e6574733f343e976ff6a1e67ef504f11e2
Isagani Lapira <lapiraisagani@gmail.com>
2024-08-27 13:11:14 +0800

    Add: working navbar and routable pages

--- elegantee/tailwind.config.js
+++ elegantee/tailwind.config.js
@@ -9,7 +9,10 @@ export default {
       colors:{
         'accent-color':'#FDA136',
         'accent-color-hovered': '#D18A36',
-        'text': '#1E1E1E'
+        'text': '#1E1E1E',
+        'active':'#FDA136',
+        'inactive':'#959595',
+        
       },
     },
   },
